813e781ed02afcfa768eb5e9767e8f1171376500
KusumLata930 <dhankharkusum0@gmail.com>
2026-09-12 12:09:00 +0530

    Add Clerk webhook integration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebHooks } from './controllers/webhooks.js'
 
 //initialise Express
 const app= express()
@@ -14,8 +15,9 @@ app.use(cors())
 
 //routes
 app.get('/',(req,res)=> res.send("API Working"))
+app.post('/clerk',express.json(),clerkWebHooks)
 
-//port
+//port 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
